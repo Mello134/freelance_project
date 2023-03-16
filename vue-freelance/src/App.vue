886b44f9d8d1021e9a364fe2627535afedd34c41
Mello134 <elproject220@yandex.ru>
2023-03-16 15:53:18 +0300

    '10.1_main.js_App.vue_axios_and_BootstrapVeu_unstall+template'

--- vue-freelance/src/App.vue
+++ vue-freelance/src/App.vue
@@ -1,8 +1,35 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <header class="bg-dark"> <!--    Навбар    -->
+      <b-navbar class="w-75 mx-auto" type="dark" variant="dark">
+        <b-navbar-brand>Freelance Stock</b-navbar-brand>
+        <b-navbar-nav>
+          <b-nav-item href="#">Главная</b-nav-item>
+          <b-nav-item href="#">Заказы</b-nav-item>
+          <b-nav-item href="#">Услуги</b-nav-item>
+          <b-nav-item href="#">О нас</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </header> <!--    /Навбар    -->
+    <div class="w-75 ms-auto"> <!--   контент(роутер)    -->
+      <router-view/>
+      <p>Бла бла бла</p>
+      <p>Чёртов аксиос</p>
+    </div> <!--    /контент    -->
+    <footer>
+      <b-jumbotron bg-variant="dark" text-variant="white">
+        <div class="w-75 mx-auto">
+          <div class="row">
+            <div class="col-4 text-left">© Freelance Stock, 2023</div>
+            <div class="col-4 text-center">Контакты</div>
+            <div class="col-4 text-right">О нас</div>
+          </div>
+        </div>
+      </b-jumbotron>
+    </footer>
+  </div> <!--    /id='app'    -->
+
 </template>
 
 <script>
@@ -12,12 +39,13 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.navbar, .navbar-brand {
+  font-size: 16pt;
 }
+
+footer {
+  font-size: 20pt;
+}
+
 </style>
--- vue-freelance/src/main.js
+++ vue-freelance/src/main.js
@@ -1,15 +1,28 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+
+/* регистрирую все модули и плагины */
+// axios
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+// BootstrapVue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+/* плагины установленные в начале vue create vue-freelance */
+import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+/* модули и плагины доступные для всего проекта */
+// axios
+Vue.use(VueAxios, axios);
+// BootstrapVue
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>',
-});
+  render: (h) => h(App),
+  axios,
+}).$mount('#app');
